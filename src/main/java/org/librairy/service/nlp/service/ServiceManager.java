068a1ab73b962d0eb467695b25311506ecacb38d
Carlos Badenes <cbadenes@gmail.com>
2018-05-11 12:58:55 +0200

    cache for pre-trained models

--- src/main/java/org/librairy/service/nlp/service/ServiceManager.java
+++ src/main/java/org/librairy/service/nlp/service/ServiceManager.java
@@ -1,5 +1,8 @@
 package org.librairy.service.nlp.service;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import org.librairy.service.nlp.annotators.StanfordAnnotatorEN;
 import org.librairy.service.nlp.annotators.StanfordLemmaTokenizer;
 import org.slf4j.Logger;
@@ -8,9 +11,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
-import java.io.IOException;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutionException;
 
 /**
  * @author Badenes Olmedo, Carlos <cbadenes@fi.upm.es>
@@ -23,39 +26,53 @@ public class ServiceManager {
     @Value("#{environment['RESOURCE_FOLDER']?:'${resource.folder}'}")
     String resourceFolder;
 
-    Map<String,IXAService> ixaServices;
-    Map<String,CoreNLPService> coreServices;
+    LoadingCache<String, IXAService> ixaModels;
+
+    LoadingCache<String, CoreNLPService> coreModels;
 
     @PostConstruct
     public void setup(){
-        ixaServices     = new ConcurrentHashMap<>();
-        coreServices    = new ConcurrentHashMap<>();
+        ixaModels = CacheBuilder.newBuilder()
+                .maximumSize(100)
+                .build(
+                        new CacheLoader<String, IXAService>() {
+                            public IXAService load(String key) {
+                                LOG.info("Initializing IXA service for thread: " + key);
+                                IXAService ixaService = new IXAService(resourceFolder);
+                                ixaService.setup();
+                                return ixaService;
+                            }
+                        });
+
+        coreModels = CacheBuilder.newBuilder()
+                .maximumSize(100)
+                .build(
+                        new CacheLoader<String, CoreNLPService>() {
+                            public CoreNLPService load(String key) {
+                                LOG.info("Initializing CoreNLP service for thread: " + key);
+                                CoreNLPService coreService = new CoreNLPService();
+                                coreService.setAnnotator(new StanfordAnnotatorEN());
+                                coreService.setTokenizer(new StanfordLemmaTokenizer());
+                                return coreService;
+                            }
+                        });
     }
 
     public IXAService getIXAService(Thread thread) {
-
-        String id = "thread"+thread.getId();
-        if (!ixaServices.containsKey(id)){
-            LOG.info("Initializing IXA service for thread: " + id);
-            IXAService ixaService = new IXAService(resourceFolder);
-            ixaService.setup();
-            ixaServices.put(id,ixaService);
+        try {
+            return ixaModels.get("thread"+thread.getId());
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
         }
-        return ixaServices.get(id);
-
     }
 
     public CoreNLPService getCoreService(Thread thread) {
 
-        String id = "thread"+thread.getId();
-        if (!coreServices.containsKey(id)){
-            LOG.info("Initializing CoreNLP service for thread: " + id);
-            CoreNLPService coreService = new CoreNLPService();
-            coreService.setAnnotator(new StanfordAnnotatorEN());
-            coreService.setTokenizer(new StanfordLemmaTokenizer());
-            coreServices.put(id,coreService);
+        try {
+            return coreModels.get("thread"+thread.getId());
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
         }
-        return coreServices.get(id);
 
     }
 
